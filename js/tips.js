18b8b834fe5eec5091dc95439e17768459b75444
GuChaiGui <76016204+GuChaiGui@users.noreply.github.com>
2020-12-14 23:34:38 +0100

    commit projet

---
+++ js/tips.js
@@ -0,0 +1,33 @@
+const app = document.getElementById('root');
+
+const logo = document.createElement('img');
+logo.src = 'https://i.postimg.cc/8zhTNFV8/hiclipart-com.png';
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(logo);
+app.appendChild(container);
+
+var request = new XMLHttpRequest();
+request.open('GET', 'https://ghibliapi.herokuapp.com/species', true);
+request.onload = function () {
+
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(specie => {
+      
+
+      container.appendChild(card);
+      card.appendChild(h1);
+      card.appendChild(p);
+    });
+  } else {
+    const errorMessage = document.createElement('marquee');
+    errorMessage.textContent = `Gah, it's not working!`;
+    app.appendChild(errorMessage);
+  }
+}
+
+request.send();
